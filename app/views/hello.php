54c3713e5871eb777fdd928082a9a2ca8eeaa12f
fabricio <damian13.fg@gmail.com>
2017-08-30 18:09:35 -0300

    Venteo

---
+++ app/views/hello.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+
+</head>
+<div class="container">
+    <div class="wrapper">
+        <form class="form-signin">
+            <h3 class="form-signin-heading">
+
+                <img src="http://www.logospike.com/wp-content/uploads/2015/11/Logo_Image_01.png" style="width:100px;height:100px" alt="Alternate Text" />
+                <br />
+                <b style="color:#c7c7c7 !important">Company Name</b>
+            </h3>
+
+            <input type="text" class="form-control" name="u_name" placeholder="Username" required="" autofocus="" /><br />
+            <input type="password" class="form-control" name="u_pass" placeholder="Password" required="" />
+
+            <button class="btn btn-lg btn-primary btn-block" name="Submit" value="Login" type="Submit">Login</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
